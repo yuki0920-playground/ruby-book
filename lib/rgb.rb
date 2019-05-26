57c8f693f2b01aece8e19f61a76784bdbfee210e
yuki0920 <ywsep20@gmail.com>
2019-05-27 08:19:56 +0900

    rgb変換の演習を追加

---
+++ lib/rgb.rb
@@ -0,0 +1,26 @@
+def to_hex(r, g, b)
+  # '#' +
+  # r.to_s(16).rjust(2, '0') +
+  # g.to_s(16).rjust(2, '0') +
+  # b.to_s(16).rjust(2, '0')
+  hex = '#'
+  [r, g, b].each do |n|
+    hex += n.to_s(16).rjust(2, '0')
+  end
+  hex
+end
+
+def to_ints(hex)
+  # r = hex[1..2]
+  # g = hex[3..4]
+  # b = hex[5..6]
+  # ints = []
+  # [r, g, b].each do |s|
+  #   ints << s.hex
+  # end
+  # ints
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = [r, g, b].map { |s| s.hex}
+end
